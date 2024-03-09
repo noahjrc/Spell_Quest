b9783b14b8ec0c0aa9e8aae1f7733b7608c188f0
Noah Colbourne <noahcolbourne2002@gmail.com>
2024-03-09 15:22:06 -0330

    Text-to-Speech with C# .Net
    Added the text to speech code, with class commented out for later use

---
+++ TTS_Test/TTS_Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+class TTS_Test
+{
+    static void Main(string[] args)
+    {
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Speak("Hello");
+        }
+        else
+        {
+            Console.WriteLine("Speech synthesis is only supported on Windows.");
+        }
+    }
+}
+
+
+/*
+using System;
+using System.Speech.Synthesis;
+
+public class TTS_Test
+{
+    public void SpeakText(string text)
+    {
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Speak(text);
+        }
+        else
+        {
+            Console.WriteLine("Speech synthesis is only supported on Windows.");
+        }
+    }
+}
+*/
